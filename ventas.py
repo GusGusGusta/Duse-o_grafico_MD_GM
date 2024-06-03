5b6dc3a456b909e90372351b1ed6403e5ea4e9eb
GusGusGusta <1993gustavo1993@gmail.com>
2024-06-03 16:05:35 -0400

    1

--- ventas.py
+++ ventas.py
@@ -24,7 +24,7 @@ def insertToDb(ventas):
     cursor = conexion.cursor()
     cursor.execute('''
     INSERT INTO ventas (fecha, producto, categoria, precio, cantidad, total)
-    VALUES (?, ?, ?, ?, ?, ?)
+    VALUES ('2024-01-20', 'Pan Integral', 'Panadería', 1400, 1, 1400*1)
     ''', ventas)
     conexion.commit()
     conexion.close()
@@ -50,29 +50,30 @@ def buscar_por_fecha(fecha):
 crear_tabla()
 
 # Datos de ejemplo
-ventas = [
-('2024-01-20', 'Pan Integral', 'Panadería', 1400, 1, 1400*1),
-('2024-01-21', 'Arroz', 'Abarrotes', 990, 1, 990*1),
-('2024-01-22', 'Detergente Líquido', 'Hogar', 3500, 1, 3500*1),
-('2024-02-20', 'Microondas', 'Electrodoméstico', 49990, 1, 49990*1),
-('2024-02-21', 'Lámpara LED', 'Eléctrico', 4990, 1, 4990*1),
-('2024-02-22', 'Harina', 'Abarrotes', 990, 1, 990*1),
-('2024-03-20', 'Café Instantáneo', 'Abarrotes', 2590, 1, 2590*1),
-('2024-03-21', 'Sartén Antiadherente', 'Hogar', 29990, 2, 29990*2),
-('2024-03-2', 'Licuadora', 'Electrodoméstico', 29990, 2, 22990*2),
-('2024-04-20', 'alargador de Enchufes', 'Eléctrico', 3990, 2, 3990*2),
-('2024-04-21', 'Fideos', 'Abarrotes', 990, 2, 990*2),
-('2024-04-22', 'Tarro Salmon', 'Abarrotes', 1200, 2, 1200*2),
-('2024-05-20', 'Cubre Camas', 'Hogar', 19990, 2, 19990*2),
-('2024-05-21', 'Sabanas', 'Hogar', 20990, 2, 20990*2),
-('2024-05-22', 'Pan Molde', 'Panadería', 1300, 2, 1300*2),
-('2024-06-01', 'Galleta', 'Panadería', 1300, 2, 1300*2),
-('2024-06-01', 'Palta', 'Verdureria', 1300, 2, 1300*2),
-('2024-06-01', 'Bebida', 'Bebidas', 1300, 2, 1300*2),
-('2024-06-01', 'Leche', 'Abarrotes', 1300, 2, 1300*2),
-('2024-06-01', 'Jamon', 'Fiambreria', 1300, 2, 1300*2)
 
+ventas = [
+    ('2024-01-20', 'Pan Integral', 'Panadería', 1400, 1, 1400*1),
+    ('2024-01-21', 'Arroz', 'Abarrotes', 990, 1, 990*1),
+    ('2024-01-22', 'Detergente Líquido', 'Hogar', 3500, 1, 3500*1),
+    ('2024-02-20', 'Microondas', 'Electrodoméstico', 49990, 1, 49990*1),
+    ('2024-02-21', 'Lámpara LED', 'Eléctrico', 4990, 1, 4990*1),
+    ('2024-02-22', 'Harina', 'Abarrotes', 990, 1, 990*1),
+    ('2024-03-20', 'Café Instantáneo', 'Abarrotes', 2590, 1, 2590*1),
+    ('2024-03-21', 'Sartén Antiadherente', 'Hogar', 29990, 2, 29990*2),
+    ('2024-03-2', 'Licuadora', 'Electrodoméstico', 29990, 2, 22990*2),
+    ('2024-04-20', 'alargador de Enchufes', 'Eléctrico', 3990, 2, 3990*2),
+    ('2024-04-21', 'Fideos', 'Abarrotes', 990, 2, 990*2),
+    ('2024-04-22', 'Tarro Salmon', 'Abarrotes', 1200, 2, 1200*2),
+    ('2024-05-20', 'Cubre Camas', 'Hogar', 19990, 2, 19990*2),
+    ('2024-05-21', 'Sabanas', 'Hogar', 20990, 2, 20990*2),
+    ('2024-05-22', 'Pan Molde', 'Panadería', 1300, 2, 1300*2),
+    ('2024-06-01', 'Galleta', 'Panadería', 1300, 2, 1300*2),
+    ('2024-06-01', 'Palta', 'Verdureria', 1300, 2, 1300*2),
+    ('2024-06-01', 'Bebida', 'Bebidas', 1300, 2, 1300*2),
+    ('2024-06-01', 'Leche', 'Abarrotes', 1300, 2, 1300*2),
+    ('2024-06-01', 'Jamon', 'Fiambreria', 1300, 2, 1300*2)
 ]
+
 
 
 
